5983aaab04c176c5dc9313aa86d9193930b67beb
HuyDuong <64969059+Deamon227@users.noreply.github.com>
2024-01-26 11:58:34 +0700

    Add files via upload

---
+++ DataAccess/Repository/IMemberRepository.cs
@@ -0,0 +1,25 @@
+﻿
+using BussinessObject;
+namespace MyStoreWinApp
+
+{
+     public interface IMemberRepository
+    {
+        IEnumerable<MemberObject> GetMembers();
+
+        MemberObject GetMemberID(int memberId);
+
+        void InsertMember(MemberObject member);
+
+        void DeleteMember(int memberId);
+
+        void UpdateMember(MemberObject member);
+
+        IEnumerable<MemberObject> SortNameDesc();
+
+        IEnumerable<MemberObject> FilterMemberByCountry(string country);
+
+        IEnumerable<MemberObject> FilterMemberByCity(string city);
+
+    }
+}
